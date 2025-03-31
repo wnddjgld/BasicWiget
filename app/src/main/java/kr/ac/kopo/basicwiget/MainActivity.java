b6c3eba90e66fec32e2ec6311dfd2fc35298f8ad
wnddjgld <wnddjgld@gmail.com>
2025-03-31 15:21:55 +0900

    TextView의 속성들을 JAVA에서 설정

--- app/src/main/java/kr/ac/kopo/basicwiget/MainActivity.java
+++ app/src/main/java/kr/ac/kopo/basicwiget/MainActivity.java
@@ -1,6 +1,8 @@
 package kr.ac.kopo.basicwiget;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +15,23 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.activity_main2);
+
+
+        TextView text1 = findViewById(R.id.text1);
+        TextView text2 = findViewById(R.id.text2);
+        TextView text3 = findViewById(R.id.text3);
+
+        text1.setTextSize(20);
+        text1.setTextColor(Color.MAGENTA);
+        text1.setText("한국폴리텍대학");
+
+        text2.setTextSize(30);
+        text2.setTextColor(Color.BLUE);
+        text2.setText("인공지능소프트웨어");
+
+        text3.setText("SingleLine속성SingleLine속성SingleLine속성SingleLine속성SingleLine속성SingleLine속성SingleLine속성");
+        text3.setSingleLine(true);
+
     }
 }
